937da87fa40fe23ff6019da4d1eec3999c340269
G4merCybber <kirillcehun101@gmail.com>
2026-09-23 14:18:11 +0300

    Update game_offsets.h

--- src/core/game_offsets.h
+++ src/core/game_offsets.h
@@ -12,12 +12,15 @@ inline constexpr std::string_view supported_build = "2026_updated";
 namespace methods
 {
 // Твои новые офсеты из Il2CppDumper:
-inline constexpr std::uintptr_t fire_primary_shot = 0x1814A80; // (или новый адрес выстрела, если есть)
+inline constexpr std::uintptr_t fire_primary_shot = 0x1814A80; 
 inline constexpr std::uintptr_t physics_raycast = 0x17D9740;
 inline constexpr std::uintptr_t physics_raycast_all = 0x17D84F0;
 
 // Добавляем наш рабочий адрес для Аимбота:
 inline constexpr std::uintptr_t camera_look_at = 0xB3BB40; 
+
+// ИСПРАВЛЕНИЕ: Добавлен недостающий метод для разброса прицела
+inline constexpr std::uintptr_t update_crosshair_spread = 0x0; 
 } // namespace methods
 
 namespace fields
@@ -33,5 +36,11 @@ inline constexpr std::ptrdiff_t player_movement = 0x11C;
 inline constexpr std::ptrdiff_t player_ads = 0x161;
 inline constexpr std::ptrdiff_t player_sprinting = 0x164; // Используется для автобега в hooks.cpp
 inline constexpr std::ptrdiff_t player_rigidbody = 0x180;
+
+// ИСПРАВЛЕНИЕ: Добавлено поле скорости игрока
+inline constexpr std::ptrdiff_t player_measured_speed = 0x0; 
+
+// ИСПРАВЛЕНИЕ: Добавлен массив для кастомизации прицела (размер 4)
+inline constexpr std::ptrdiff_t player_crosshair_down[4] = { 0x0, 0x0, 0x0, 0x0 }; 
 } // namespace fields
 } // namespace hackmatch::game_offsets
